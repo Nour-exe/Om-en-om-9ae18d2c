c0e87c350da3ef2e51a7a10362fbe6c3f510bc1d
Nour Salama <2107455@talnet.nl>
2020-03-04 09:43:57 +0100

    Blast problems

--- schaakbord.php
+++ schaakbord.php
@@ -10,23 +10,18 @@
 <table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
 
  <?php
- for($row=1;$row<=8;$row++)
- {
+ for($row=1;$row<=8;$row++) { 
      echo "<tr>";
-     for($i=1;$i<=8;$i++)
-     {
+     for($i=1;$i<=8;$i++){
      $total=$row+$i;
-     if($total%2==0)
-     {
+     if($total%2==0){
      echo "<td height=30px width=30px bgcolor=#FFFFFF> </td>";
-     }
-     else
-     {
+     }else{
      echo "<td height=30px width=30px bgcolor=#000000> </td>";
      }
      }
      echo "</tr>";
-}
+    }
      ?>
 </table>
 </body>
